2cf8e0997c709de58d5365e55fc9a06739a1df71
olivier <ovanlier@outlook.com>
2022-12-10 12:51:18 +0100

    update

--- day9/day9.py
+++ day9/day9.py
@@ -1,3 +1,4 @@
+import numpy as np
 #Objective Simulate your complete hypothetical series of motions. 
 # How many positions does the tail of the rope visit at least once?
 
@@ -5,9 +6,9 @@ def main():
     print("day9")
     file =open("input.txt","r")
     lines = file.readlines()
-    head = [0,0] #start position of the head
-    tail = [0,0] #start position of the tail
-    tailPositions = set()
+    head = np.array([0,0]) #start position of the head
+    tail = np.array([0,0]) #start position of the tail
+    tailPositions = list() #keep track of 
     for line in lines:
         direction, steps = line.split()
         steps = int(steps)
@@ -22,20 +23,29 @@ def main():
             elif(direction=='R'):
                 head[0]+=1
             
+            diff = head - tail
+            #print(diff)
+
             #x distance larger than 2? tail follows 1 position in that direction
-            diff=head[0]-tail[0]
-            if(abs(diff) == 2):
-                tail[0]+=(diff/2)
+            if( np.equal(abs(diff),np.array([2,0])).all() ):
+                print(tail)
             
             #y distance larger than 2? tail follows 1 position in that direction
-            diff=head[1]-tail[1]
-            if(head[1]-tail[1]==2):
-                tail[1]+=(diff/2)
-                
-            tailPositions.add(tail) # save talk position
+            if( np.equal(abs(diff),np.array([0,2])).all() ):
+                tail[1]+=diff[1]/2
+
+            if( np.equal(abs(diff),np.array([1,2])).all() or np.equal(abs(diff),np.array([2,1])).all() ):
+                tail[0]+=diff[0]/2
+                tail[1]+=diff[1]/2
+                print(tail)
+
+            element = np.copy(tail)
+            tailPositions.append(element)
             
-    amountofpositions = len(tailPositions)
-    print("There are {amountofpositions} positions that the tail visitied at least once")
+            
+    print(tailPositions)
+    amountofpositions =  len(set(tuple(i) for i in tailPositions))#eleminate duplicates and count unique positions of tail
+    print(f"There are {amountofpositions} positions that the tail visited at least once")
             
                 
         
